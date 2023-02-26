cbf3156522495c236e9391df147d92ddc5eb3dd6
nikl1425 <nmhjort7@gmail.com>
2023-02-26 20:09:31 +0100

    working on navbar

--- src/common/components/navbar.tsx
+++ src/common/components/navbar.tsx
@@ -1,17 +1,46 @@
-import React, {FC} from "react";
+import React, { FC } from "react";
 import Link from "next/link";
+
+
+
+const NavItem: FC<{ linkRef: string, name: string }> = ({ linkRef, name }) => {
+    return (
+        <>
+            <li className="inline mx-2">
+                <Link href={linkRef}>
+                    {name}
+                </Link>
+            </li>
+        </>
+    )
+}
+
+const NavLinkContainer: FC = () => {
+    return (
+        <div className=" mx-3 my-auto">
+            <ul className="inline">
+                <NavItem name="about" linkRef="/about" />
+                <NavItem name="course" linkRef="/course" />
+            </ul>
+        </div>
+    )
+}
+
+const NavLogo : FC = () => {
+    return (
+        <>
+        </>
+    )
+}
 
 const NavBar: FC = () => {
     return (
-        <div>
-            <ul>
-                <li>
-                    <Link href="/about">about</Link>
-                </li>
-                <li>
-                    <Link href="/course">Course</Link>
-                </li>
-            </ul>
+        <div className=" h-12 bg-blue-100 flex">
+            <div className="flex-intial">
+
+            </div>
+            <div className="flex-auto"></div>
+            <NavLinkContainer />
         </div>
     )
 }
