78075d184f0029b04f733352424bd614b55b3feb
nikl1425 <nmhjort7@gmail.com>
2023-02-27 23:12:06 +0100

    link hover effects

--- src/common/components/navbar.tsx
+++ src/common/components/navbar.tsx
@@ -7,7 +7,7 @@ import Book from 'public/book.png';
 const NavItem: FC<{ linkRef: string, name: string }> = ({ linkRef, name }) => {
     return (
         <>
-            <li className="inline mx-2">
+            <li className="inline mx-2 hover:before:scale-x-100 hover:before:origin-left relative before:w-full before:h-1 before:origin-right before:transition-transform before:duration-300 before:scale-x-0 before:bg-green-400 before:absolute before:left-0 before:bottom-0 py-1">
                 <Link href={linkRef}>
                     {name}
                 </Link>
@@ -19,7 +19,7 @@ const NavItem: FC<{ linkRef: string, name: string }> = ({ linkRef, name }) => {
 const NavLinkContainer: FC = () => {
     return (
         <div className=" h-full flex items-center ">
-            <ul className="inline">
+            <ul className="inline px-4 font-bold">
                 <NavItem name="about" linkRef="/about" />
                 <NavItem name="course" linkRef="/course" />
             </ul>
@@ -35,7 +35,7 @@ const NavCompanyWrapper: FC = () => {
     return (
         <div className=" justify-center mx-3 flex flex-row h-full right">
             <NavLogo />
-            <h1 className="  my-auto">Learn4Fun</h1>
+            <h1 className=" px-4 text-lg my-auto">Learn<span className=" font-bold text-green-400 text-xl px-0 mx-0">4</span>Fun</h1>
         </div>
     )
 }
