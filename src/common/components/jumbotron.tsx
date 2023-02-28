77ff259ab359fc8823b478adf839b582cbd355ae
nikl1425 <nmhjort7@gmail.com>
2023-03-01 00:51:36 +0100

    working on my jumbotron

--- src/common/components/jumbotron.tsx
+++ src/common/components/jumbotron.tsx
@@ -7,24 +7,33 @@ type JumboButtonProps = {
     func: () => void
 }
 
+type JumboStyle = {
+    centerRelativeTo: 'screen' | 'wrapper'
+}
+
 type JumboTronProps = {
     title: string
     subTitle?: string 
-    buttons: []
+    buttons: JumboButtonProps[]
     bannerIcon?: string
     bannerImg?: string
+    style: JumboStyle
 }
 
-const JumboTron: FC<JumboTronProps> = ({title, subTitle}) => {
+const JumboTron: FC<JumboTronProps> = ({title, subTitle, buttons, bannerIcon, bannerImg, style}) => {
     return (
         <div className="w-full h-full bg-white shadow-2xl rounded-lg mx-auto text-center flex">
             <div>
 
             </div>
-            <div className=" mx-auto my-auto grow">
+            <div className=" mx-auto my-auto ">
                 <h2 className="text-3xl leading-9 font-bold tracking-tight text-gray-800 sm:text-4xl sm:leading-10">
+                    {title}
                     Start studying with us
                 </h2>
+                <h4>
+                    {subTitle}
+                </h4>
                 <div className="mt-4 mb-4 flex justify-center">
                     <div className="inline-flex rounded-md bg-blue-500 shadow">
                         <a href="#" className="text-gray-200 font-bold py-2 px-6">
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,11 +3,24 @@ import { Inter } from 'next/font/google'
 import JumboTron from '../common/components/jumbotron'
 const inter = Inter({ subsets: ['latin'] })
 
+
 export default function Home() {
+
+
+
   return (
     <>
       <div className=' h-full'>
-        <JumboTron />
+        <JumboTron 
+        title='Start studying with us' 
+        style={{centerRelativeTo: 'screen'}}
+        buttons={
+          [
+            { text: 'Explore', func: () => {}, color: 'green'},
+            { text: 'Intro Video', func: () => {}, color: 'yellow'}
+          ]
+        }
+        />
       </div>
     </>
   )
