fe24f2f3b9e4886609e48f9b2707e1d2a6c898fa
niklas@astne.dk <niklas@astne.dk>
2023-03-10 00:10:31 +0100

    btn work

--- src/common/components/button.tsx
+++ src/common/components/button.tsx
@@ -1,19 +1,49 @@
 import React, { FC } from 'react'
 
-type ButtonProps = {
-    type: 'button',
-    onclick: () => void
+type btnStyleProps = {
     bgColor: string,
     hoverColor?: string,
-    textColor: 'black' | 'white'
-    
+    textColor?: 'black' | 'white'
 }
 
-const Button: FC = () => {
+type ButtonProps = {
+    type?: 'submit' | 'reset' | 'button',
+    onclick: () => void,
+    style: btnStyleProps,
+    buttonText: string
+}
+
+const getTextColor = (val : string | undefined) => val === 'black' || val === undefined ? 'text-black' : 'text-white';
+
+const bgHoverColorMap = new Map<string, string>([
+    ['success', 'bg-green-600'],
+    ['danger', 'bg-red-600'],
+    ['alert', 'bg-yellow-400'],
+    ['info', 'bg-sky-600']
+])
+
+const bgColorMap = new Map<string, string>([
+    ['success', 'bg-green-400'],
+    ['danger', 'bg-red-500'],
+    ['alert', 'bg-yellow-300'],
+    ['info', 'bg-sky-500']
+])
+
+const btnStyle = (input: btnStyleProps) => {
+    let bg = bgColorMap.get(input.bgColor);
+    let hover = input.hoverColor != undefined ? bgHoverColorMap.get(input.hoverColor) : '';
+    let btnTxtCol = getTextColor(input.textColor)
+    return [bg, hover, btnTxtCol ].join(' ') + ' ';
+}
+
+const Button: FC<ButtonProps> = (props) => {
     return (
         <>
-            <button>
-
+            <button
+            type={props.type}
+            onClick={props.onclick}
+            className={btnStyle(props.style) + ''}>
+                {props.buttonText}
             </button>
         </>
     )
