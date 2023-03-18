730b2cd96957a608e450c156d538413da9c91aba
niklas@astne.dk <niklas@astne.dk>
2023-03-18 12:20:59 +0100

    cards

--- src/pages/course.tsx
+++ src/pages/course.tsx
@@ -1,7 +1,8 @@
 import { PrismaClient } from "@prisma/client";
 import { CourseProps } from "@/common/models";
 import { FaSearch } from "react-icons/fa";
-import {AiOutlineSearch} from "react-icons/ai";
+import { AiOutlineSearch } from "react-icons/ai";
+import Card from "@/common/components/card";
 
 
 export async function getStaticProps() {
@@ -29,25 +30,32 @@ const Course = ({ courses }: CourseProps) => {
                     </h2>
                     <div className="mt-8 ">
                         <div className="inline-flex bg-white shadow rounded-lg">
-                            <div className="flex justify-center mt-1 mb-1 mr-1 "> 
-                            <AiOutlineSearch size={20} fontWeight={400} className="text-gray-400 m-auto ml-2"/>
-                            <input className="w-60 pl-2 border-transparent focus:outline-none" placeholder="Søg efter kurser..." />
+                            <div className="flex justify-center mt-1 mb-1 mr-1 ">
+                                <AiOutlineSearch size={20} fontWeight={400} className="text-gray-400 m-auto ml-2" />
+                                <input className="w-60 pl-2 border-transparent focus:outline-none" placeholder="Søg efter kurser..." />
                             </div>
                         </div>
                     </div>
                 </div>
 
             </div>
-            <div className="container ">
+            <div className="container mb-6">
                 <div className="w-full text-center mt-4">
                     <h1 className="font-bold text-3xl">Tilgængelige Kurser</h1>
                 </div>
 
-
-                {courses.map((course) => (
-                    <div key={course.id}>{course.title}</div>
-                ))}
+                <div className="mt-6 mb-6">
+                    <div className="flex flex-row space-x-10 justify-center">
+                        {courses.map((course) => (
+                            <div key={course.id}>
+                                {course.title}
+                                <Card title={course.title} description={course.description ? course.description : ''} />
+                            </div>
+                        ))}
+                    </div>
+                </div>
             </div>
+            <div>hej</div>
         </>
     )
 }
