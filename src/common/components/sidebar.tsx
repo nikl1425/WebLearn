4a9233ccf36a635d9dcb0365920e735f2e5c3e60
niklas@astne.dk <niklas@astne.dk>
2023-03-27 22:45:12 +0200

    pretty far with navbar

--- src/common/components/sidebar.tsx
+++ src/common/components/sidebar.tsx
@@ -3,6 +3,10 @@ import Button from "./button";
 import {RiCloseCircleLine} from 'react-icons/ri';
 import {GiHamburgerMenu} from 'react-icons/gi';
 import useStore from '../../store/store';
+import { useRouter } from "next/router";
+import Link from 'next/link';
+import { ROUTES_CONFIG } from "../constants";
+import { IconType } from "react-icons/lib";
 
 
 const UserContainer: FC<{show: boolean}> = ({show}) => {
@@ -10,7 +14,7 @@ const UserContainer: FC<{show: boolean}> = ({show}) => {
         <>
             <div className="flex items-center flex-col mt-14 mb-4">
                 <div className={`transition-width duration-700 ease-in-out ${ show ? 'w-28' : 'w-0'}`}>
-                    {show && <div className="rounded-full bg-green-300 h-28 flex mb-2">
+                    {show && <div className="rounded-full bg-green-400 h-28 flex mb-2">
                         <span className="my-auto mx-auto text-white font-bold tracking-wider text-lg">NHR</span>
                     </div>
                     }  
@@ -22,13 +26,47 @@ const UserContainer: FC<{show: boolean}> = ({show}) => {
 }
 
 const CloseOpenHandler : FC<{handler: () => void, state: boolean}> = ({handler, state}) => {
-
     return (
         <div className="w-full flex flex-row-reverse">
             <button onClick={handler} className="px-2">
                 {state === true ? <RiCloseCircleLine className="text-indigo-200" size={30}/> : <GiHamburgerMenu className="text-indigo-200" size={30}/>}
             </button>
         </div>
+    )
+}
+
+const NavLink : FC<{name: string, route: string, symbol: IconType}> = ({name, route, symbol}) => {
+    const Icon = symbol;
+
+    return (
+        <>
+            <div>
+                <Link href={"/" + route}>
+                    <div>
+                        <div>
+                            {<Icon className="text-green-400" size={25} />}
+                        </div>
+                        <span>
+                            {name}
+                        </span>
+                    </div>
+                </Link>
+            </div>
+        </>
+    )
+}
+
+const NavLinkList : FC = () => {
+    const routes = ROUTES_CONFIG;
+
+    return (
+        <>
+            <div>
+                <ul>
+                    {routes.map((route) => <NavLink name={route.name} route={route.route} symbol={route.symbol}/>)}
+                </ul>
+            </div>
+        </>
     )
 }
 
@@ -43,6 +81,7 @@ const SideBar: FC = () => {
         <div className={`h-full bg-gray-800`}>
             <CloseOpenHandler handler={sidebarHandler.setSidebarOpen} state={open} />
             <UserContainer show={sidebarHandler.sidebarOpen}/>
+            <NavLinkList />
         </div>
     )
 }
---
+++ src/common/constants.tsx
@@ -0,0 +1,12 @@
+import { GiHomeGarage } from "react-icons/gi"
+
+
+
+
+export const ROUTES_CONFIG = [
+    { name: 'Hjem', route: '', symbol: GiHomeGarage},
+    { name: 'Kurser', route: '', symbol: GiHomeGarage},
+    { name: 'Information', route: '', symbol: GiHomeGarage},
+]
+        
+
